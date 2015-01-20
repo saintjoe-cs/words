8ae3053b33a36983942d79bf94d8368fd7800c24
Brian Capouch <brianc@saintjoe.edu>
2015-01-20 03:01:45 -0500

    (So far) Allow any combination of node and d8 output

--- words.js
+++ words.js
@@ -2,18 +2,31 @@ if (typeof(process) == "object") {
   var print = function(args) {
 	console.log(args);
 	}
+  var write = function(args) {
+	process.stdout.write(args);
+	}
+  } else {
+  console = new Object();
+  console.log = function(args) {
+    print(args);
+	}
+  process = new Object();
+  process.stdout = new Object();
+  process.stdout.write = function(args) {
+    write(args);
+    }
   }
 
 var words = ["My","dog","has","enormous","fleas"];
 
 var forwords = function(arr) {
-  print('Words done forwards:');
+  console.log('Words done forwards:');
   for ( var i = 0; i < arr.length; ++i ) 
 	print(arr[i]);
   }
 
 var backwords = function(arr) {
-  print('\nWords done backwards:');
+  process.stdout.write('\nWords done backwards:\n');
   for ( var i = ( arr.length -1 ); i >= 0; i-- )
 	print(arr[i]);
   }
